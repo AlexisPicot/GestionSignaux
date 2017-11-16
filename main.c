72466dcd9ded2bcfcbf7374973ba852328a74615
alexis.picot <alexis.picot@cpe.fr>
2017-11-16 15:10:51 +0100

    Exercice 3

--- main.c
+++ main.c
@@ -3,12 +3,19 @@
 #include <signal.h>
 #include <stdbool.h>
 
-int main() {
-    printf("Pid : %d\n", getpid());
-    signal(SIGINT, SIG_IGN);
-    while (true) {
-        pause();
-    }
+bool fin = false;
 
+void sigintHandler(int i) {
+    puts("Signal SIGINT reçu, mise à vrai du booléen FIN");
+    fin = true;
 }
 
+int main() {
+    printf("Pid : %d\n", getpid());
+    signal(SIGINT, sigintHandler);
+    while (!fin) {
+        pause();
+    }
+    puts("Fin de la boucle, interruption du programme");
+
+}
