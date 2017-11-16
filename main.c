938feb17025013053c1aebb207231a8f560bb805
alexis.picot <alexis.picot@cpe.fr>
2017-11-16 16:24:16 +0100

    Exercice 7

--- main.c
+++ main.c
@@ -2,25 +2,51 @@
 #include <zconf.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-bool fin = false;
 
-void sigchldHandler(int i) {
-    puts("Signal SIGCHLD reçu !");
+void sigusrHandler(int i) {
+    switch (i) {
+        case SIGUSR1: {
+            puts("Signal SIGUSR1 reçu ! Execution de la boucle infinie");
+            break;
+        }
+        case SIGUSR2: {
+            puts("Signal SIGUSR2 reçu ! On arrete le programme fils");
+            exit(i);
+        }
+    }
 }
 
+
 int main() {
-    signal(SIGCHLD, sigchldHandler);
+
+    signal(SIGUSR1, sigusrHandler);
+    signal(SIGUSR2, sigusrHandler);
 
     printf("Pid : %d\n", getpid());
     int pidChild = fork();
     if (!pidChild) {
+        printf("Pid : %d\n", getpid());
         while (true) {
+            pause();
             puts("Boucle infinie");
             sleep(1);
         }
+
+
     } else {
-        sleep(3);
-        execlp("kill", "kill", "-SIGKILL", pidChild);
+        for (int i = 0; i < 5; ++i) {
+            sleep(1);
+            //kill(pidChild,SIGUSR1);
+            switch (i) {
+                case 2:
+                case 4:
+                    kill(pidChild, SIGUSR1);
+                    break;
+            }
+
+        }
+        kill(pidChild, SIGUSR2);
     }
 }
