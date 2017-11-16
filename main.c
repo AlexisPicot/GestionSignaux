d7b0f3b57788ce1120f5999de1f7cc81532d2978
alexis.picot <alexis.picot@cpe.fr>
2017-11-16 15:07:29 +0100

    Exercice 1

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <zconf.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void sigintHandler(int i) {
+    puts("Signal SIGINT reçu, interruption du programme");
+    exit(1);
+}
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    printf("Pid : %d\n", getpid());
+    signal(SIGINT, sigintHandler);
+    while (true) {
+        pause();
+    }
+
 }
