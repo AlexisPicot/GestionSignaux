966c86e0583853f026e6d1a8d789aeaa9f769d20
alexis.picot <alexis.picot@cpe.fr>
2017-11-16 15:52:42 +0100

    Exercice 6

--- main.c
+++ main.c
@@ -2,25 +2,32 @@
 #include <zconf.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 bool fin = false;
 
+void sigintHandler(int i) {
+    puts("Signal SIGINT reçu !");
+    exit(i);
+}
 void sigchldHandler(int i) {
-    puts("Signal SIGCHLD reçu !");
+    puts("Signal SIGCHLD reçu, le fils est mort \\o/ !");
 }
 
 int main() {
-    signal(SIGCHLD, sigchldHandler);
+    signal(SIGINT, sigintHandler);
 
     printf("Pid : %d\n", getpid());
     int pidChild = fork();
     if (!pidChild) {
+        printf("Pid : %d\n", getpid());
         while (true) {
             puts("Boucle infinie");
             sleep(1);
         }
     } else {
-        sleep(3);
-        execlp("kill", "kill", "-SIGKILL", pidChild);
+        signal(SIGCHLD, sigchldHandler);
+        pause();
+
     }
 }
