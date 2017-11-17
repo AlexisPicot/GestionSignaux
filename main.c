24403fa57ac93d5f3cb03df1c9054534f268b5d5
alexis.picot <alexis.picot@cpe.fr>
2017-11-17 15:16:25 +0100

    V1

--- main.c
+++ main.c
@@ -5,14 +5,17 @@
 #define CLE 1
 
 
-void advance_spinner() {
+void simulerProcessus(int sec) {
     static char bars[] = {'/', '-', '\\', '|'};
     static int nbars = sizeof(bars) / sizeof(char);
     static int pos = 0;
+    for (int i = 0; i < sec * 2; ++i) {
+        usleep(500000);
+        printf("%c\r", bars[pos]);
+        fflush(stdout);
+        pos = (pos + 1) % nbars;
+    }
 
-    printf("%c\r", bars[pos]);
-    fflush(stdout);
-    pos = (pos + 1) % nbars;
 }
 
 
@@ -22,10 +25,7 @@ int main() {
     printf("Creation du sémaphore d'identificateur %d\n", sem);
     if (fork() == 0) {
         puts("Je suis le fils, j'effectue un traitement.");
-        for (int i = 0; i < 15; ++i) {
-            usleep(300000);
-            advance_spinner();
-        }
+        simulerProcessus(4);
         puts("J'ai fini !");
         V(sem);
         exit(0);
@@ -33,10 +33,7 @@ int main() {
         puts("Je suis le père et j'attends que mon fils ait fini son traitement");
         P(sem);
         puts("Je suis le père, j'effectue un traitement.");
-        for (int i = 0; i < 15; ++i) {
-            usleep(300000);
-            advance_spinner();
-        }
+        simulerProcessus(4);
         puts("J'ai fini !");
         sem_delete(sem);
     }
