515b2d0eca1d17bf96ad1d887b1439590deca787
alexis.picot <alexis.picot@cpe.fr>
2017-11-17 15:30:56 +0100

    V1

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #define CLE 1
 
 
+void P1();
+
+void P2();
+
 void simulerProcessus(int sec) {
     static char bars[] = {'/', '-', '\\', '|'};
     static int nbars = sizeof(bars) / sizeof(char);
@@ -23,20 +27,29 @@ int main() {
     int sem;
     sem = sem_create(CLE, 0);
     printf("Creation du sémaphore d'identificateur %d\n", sem);
-    if (fork() == 0) {
-        puts("Je suis le fils, j'effectue un traitement.");
-        simulerProcessus(4);
-        puts("J'ai fini !");
-        V(sem);
-        exit(0);
-    } else {
-        puts("Je suis le père et j'attends que mon fils ait fini son traitement");
-        P(sem);
-        puts("Je suis le père, j'effectue un traitement.");
-        simulerProcessus(4);
-        puts("J'ai fini !");
-        sem_delete(sem);
-    }
+    if (fork())
+        if (fork() == 0) {
+            P2();
+        } else {
+            P1();
+            sem_delete(sem_create(CLE, 0));
+        }
+}
+
+void P2() {
+    puts("Je suis le fils, j'effectue un traitement.");
+    simulerProcessus(4);
+    puts("J'ai fini !");
+    V(sem_create(CLE, 0));
+    exit(0);
+}
+
+void P1() {
+    puts("Je suis le père et j'attends que mon fils ait fini son traitement");
+    P(sem_create(CLE, 0));
+    puts("Je suis le père, j'effectue un traitement.");
+    simulerProcessus(4);
+    puts("J'ai fini !");
 }
 
 
